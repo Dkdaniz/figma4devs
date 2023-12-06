623dca6a38c6e3dd6bf32f8b2b92951c2e0d50bf
Daniel Marques <daniel@descentralizado.com.br>
2023-12-06 19:57:43 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Figma4Devs
+
+In this project was use `style-dictionary` and `git-actions` were used to create a design system deployment automation in NPM.
+
+## Build Tokens Colors
+
+```
+yarn build
+```
+
+## Figma Project
+
+https://bit.ly/47HgkfG
---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 06 Dec 2023 22:36:50 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray700": "#1e1e1e",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6"
+};
